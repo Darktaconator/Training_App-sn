86a72ad94f6541d0257b196a12b574ce11b23033
silasnerad <silasnerad @gmail.com>
2024-11-26 21:08:40 -0600

    SADf

--- app/src/main/java/_training/ForSolution.java
+++ app/src/main/java/_training/ForSolution.java
@@ -6,7 +6,7 @@ public class ForSolution {
             for (int quarters = 0; quarters < 5; quarters++){
                 System.out.println("I ran in " + quarters + " quarters with " + i + " circle remaining!");
             }
-            System.out.println("I have to run "+i+" more circles today!");
+            System.out.println("I have to run "+i+" more circles today!"); 
         }
     }
 }
