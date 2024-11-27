abfce314ce8bacc567855e5104676f941d9592b9
silasnerad <silasnerad @gmail.com>
2024-11-26 21:03:00 -0600

    commit

--- app/src/main/java/_training/For.java
+++ app/src/main/java/_training/For.java
@@ -2,8 +2,15 @@ package _training;
 
 public class For {
     public static void main(String[] args) {
+<<<<<<< HEAD
+        for (int i = 0; i < 30; i++) {
+            System.out.println("I ran in " + i + " circles today!");
+        for (int quarters = 0; quarters < 5; quarters++)
+        system.out.println("I ran in" + quarters)
+=======
         for (int i = 0; i < 20; i++) {
             System.out.println("I ran in " + i + " circles today!");
+>>>>>>> 275f12f9752ac63f5f542583326d3c597ce05e09
         }
     }
 }
--- app/src/main/java/_training/Variables.java
+++ app/src/main/java/_training/Variables.java
@@ -6,7 +6,23 @@ public class Variables {
         int myOtherNum = 2;
 
         // Get the sum of the two numbers
-        int myFinalNum = myNum + myOtherNum;
+        int myFinalNum = 1 + 2;
         System.out.println(myFinalNum);
+        int MyFinalFinalNum = myFinalNum * 3;
+        System.out.println(MyFinalFinalNum);
+        String myText = "Hello! ";
+        String myOtherText = "Again!";
+        String myFinalText = myText + myOtherText;
+        System.out.println(myFinalText);
+        float myDecimal = 2.5f;
+        float mySecondDecimal = 5.0f;
+        float myFinalDecimal = myDecimal / mySecondDecimal;
+        System.out.println(myFinalDecimal);
+        boolean ateBreakfast = true;
+        boolean ateLunch = true;
+        boolean ateDinner = false;
+
+        System.out.println(ateBreakfast && ateLunch);
+        System.out.println(ateLunch && ateDinner);
     }
 }
